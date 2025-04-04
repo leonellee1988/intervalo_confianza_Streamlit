0dd72eb1c34b3a94166cf2926b6746c8beba3d8e
ellt1988 <leonellee2016@gmail.com>
2025-04-04 10:54:27 -0600

    Modificaciones 04042025 1055

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 import streamlit as st
 from scipy.stats import norm, t
 
+# Inicializar valores predeterminados en session_state
+if "sample_mean_or_proportion" not in st.session_state:
+    st.session_state["sample_mean_or_proportion"] = 0.0
+if "significance_level" not in st.session_state:
+    st.session_state["significance_level"] = ""
+if "standard_deviation" not in st.session_state:
+    st.session_state["standard_deviation"] = 0.0
+if "sample_size" not in st.session_state:
+    st.session_state["sample_size"] = 1
+
 # Título de la aplicación
 st.title("Confidence Interval (CI) Calculator")
 
@@ -13,21 +23,22 @@ calculation_type = st.selectbox(
 # Inputs principales
 sample_mean_or_proportion = st.number_input(
     "Enter the sample mean or proportion:",
-    #format="%.4f",
-    value=st.session_state["sample_mean_or_proportion_reset"]
+    value=st.session_state["sample_mean_or_proportion"],
+    key="sample_mean_or_proportion"
 )
 
 significance_level = st.selectbox(
     "Select the significance level (α):",
-    ["", 0.10, 0.05, 0.01]
+    ["", 0.10, 0.05, 0.01],
+    key="significance_level"
 )
 
 standard_deviation = None
 if calculation_type == "Mean":
     standard_deviation = st.number_input(
         "Enter the standard deviation:",
-        #format="%.4f",
-        value=st.session_state["standard_deviation_reset"],
+        value=st.session_state["standard_deviation"],
+        key="standard_deviation"
     )
 
 # Etiqueta condicional para el tamaño de muestra
@@ -38,9 +49,9 @@ if calculation_type == "Proportion":
 min_sample_size = 30 if calculation_type == "Proportion" else 1
 sample_size = st.number_input(
     sample_size_label,
-    #format="%d",
     min_value=min_sample_size,  # Restricción basada en el tipo de cálculo
-    value=st.session_state["sample_size_reset"]
+    value=st.session_state["sample_size"],
+    key="sample_size"
 )
 
 # Botón para calcular
